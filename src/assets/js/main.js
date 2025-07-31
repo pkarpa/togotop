9a6930d5925f577454762683a9ed97908fe10c0f
pKarpa <pavlo.karpa17@gmail.com>
2025-08-01 01:05:08 +0300

    tarifs: redemption and feedback blocks done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,3 +10,34 @@ if(carts.length){
         })
     }
 }
+
+const tarifsBtns = document.querySelectorAll("[data-tarifs-btn]");
+const tarifsBlocks = document.querySelector("[data-tarifs-blocks]");
+
+if(tarifsBtns.length){
+    for(let i = 0; i<tarifsBtns.length; i++){
+        tarifsBtns[i].addEventListener("click", e=>{
+            for(let j = 0; j < tarifsBtns.length; j++){
+                tarifsBtns[j].classList.remove("active");
+            }
+
+            tarifsBlocks.classList.remove("combo");
+            tarifsBlocks.classList.remove("redemption");
+            tarifsBlocks.classList.remove("feedback");
+
+            tarifsBtns[i].classList.add("active");
+
+            if(tarifsBtns[i].dataset.tarifsBtn === "1"){
+                tarifsBlocks.classList.add("combo");
+            }
+            else if(tarifsBtns[i].dataset.tarifsBtn === "2"){
+                tarifsBlocks.classList.add("redemption");
+            }
+            else{
+                tarifsBlocks.classList.add("feedback");
+            }
+        })
+    }
+}
+
+
